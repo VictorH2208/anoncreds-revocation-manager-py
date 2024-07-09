42f3fa9fb9f6149fa3c58be788ec55b2ad789124
VictorH2208 <2803556948@qq.com>
2024-07-10 01:49:11 +0800

    Test DCO working
    Signed-off-by: VictorH2208 <2803556948@qq.com>

--- main.py
+++ main.py
@@ -5,16 +5,6 @@ from holder import router as holder_router
 
 app = FastAPI()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-@app.post("/token")
-def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    # This example hard-codes authentication check for simplicity
-    if form_data.username == "admin" and form_data.password == "secret":
-        # Normally, return a real JWT token
-        return {"access_token": "fake-jwt-token", "token_type": "bearer"}
-    raise HTTPException(status_code=400, detail="Incorrect username or password")
-
 app.include_router(
     issuer_router,
     prefix="/issuer",
